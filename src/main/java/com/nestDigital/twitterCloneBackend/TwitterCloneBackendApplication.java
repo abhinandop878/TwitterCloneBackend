271949bfedccab3287a93c8b8ad38499ed8c6a69
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-21 12:58:17 +0530

    Twitter clone app backend

---
+++ src/main/java/com/nestDigital/twitterCloneBackend/TwitterCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestDigital.twitterCloneBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneBackendApplication.class, args);
+	}
+
+}
